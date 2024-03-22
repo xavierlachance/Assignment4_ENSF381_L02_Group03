ff1246b26912f2e0ad52c4671bdf5ebee333a14c
xavierlachance <xavier.lachance@ucalgary.ca>
2024-03-22 12:49:22 -0600

    Same

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Homepage from './components/homepage';
 
 function App() {
   return (
